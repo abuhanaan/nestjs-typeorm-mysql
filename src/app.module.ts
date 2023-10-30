e865f3d182c7f0b447d7e6a12e999a3985cc6d08
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-10-30 15:01:10 +0100

    Added User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'mustopha',
       password: 'ayinde_99',
       database: 'nestjs_tut',
+      entities: [User],
       synchronize: true,
     }),
   ],
